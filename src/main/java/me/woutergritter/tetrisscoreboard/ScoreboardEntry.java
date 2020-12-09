14fa2f70d4e2d051c5cbcaae9c05451a7b4f9cfa
Wouter Gritter <woutergritter@gmail.com>
2020-12-09 10:41:20 +0100

    Update library to be actually usable

--- src/main/java/me/woutergritter/tetrisscoreboard/ScoreboardEntry.java
+++ src/main/java/me/woutergritter/tetrisscoreboard/ScoreboardEntry.java
@@ -1,11 +1,13 @@
 package me.woutergritter.tetrisscoreboard;
 
+import java.util.Objects;
+
 public class ScoreboardEntry {
     private final String name;
     private final int score;
     private final int level;
 
-    public ScoreboardEntry(String name, int score, int level) {
+    protected ScoreboardEntry(String name, int score, int level) {
         this.name = name;
         this.score = score;
         this.level = level;
@@ -21,6 +23,21 @@ public class ScoreboardEntry {
 
     public int getLevel() {
         return level;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScoreboardEntry that = (ScoreboardEntry) o;
+        return score == that.score &&
+                level == that.level &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score, level);
     }
 
     @Override
--- src/main/java/me/woutergritter/tetrisscoreboard/TetrisScoreboard.java
+++ src/main/java/me/woutergritter/tetrisscoreboard/TetrisScoreboard.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 public class TetrisScoreboard {
     private static final char[] ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-,                                         /              ()              \".                                                                                                                                                ".toCharArray();
 
-    private ScoreboardEntry[] a_entries;
-    private ScoreboardEntry[] b_entries;
+    private ScoreboardEntry[] a_entries = null;
+    private ScoreboardEntry[] b_entries = null;
 
     public TetrisScoreboard() {
     }
@@ -37,6 +37,14 @@ public class TetrisScoreboard {
                 readScoreboardEntry(buf, 0x6E, 0x0F, 0x1D), // #2
                 readScoreboardEntry(buf, 0x74, 0x12, 0x1E)  // #3
         };
+    }
+
+    public ScoreboardEntry[] getAEntries() {
+        return a_entries;
+    }
+
+    public ScoreboardEntry[] getBEntries() {
+        return b_entries;
     }
 
     @Override
@@ -82,79 +90,3 @@ public class TetrisScoreboard {
         return buf[index];
     }
 }
-
-
-// alphabet:
-// DEC HEX LET
-// 10  0A  A
-// 11  0B  B
-// 12  0C  C
-// 13  0D  D
-// 14  0E  E
-// 15  0F  F
-// 16  10  G
-// 17  11  H
-// 18  12  I
-// 19  13  J
-// 20  14  K
-// 21  15  L
-// 22  16  M
-// 23  17  N
-// 24  18  O
-// 25  19  P
-// 26  1A  Q
-// 27  1B  R
-// 28  1C  S
-// 29  1D  T
-// 30  1E  U
-// 31  1F  V
-// 32  20  W
-// 33  21  X
-// 34  22  Y
-// 35  23  Z
-// 255 FF  -
-//
-// NAMES
-// Names are 6 bytes long and are saved as per the alphabet above ^.
-//
-// Ranges:
-// 50 - 55:  A pos 1 name
-// 56 - 5B:  A pos 2 name
-// 5C - 61:  A pos 3 name
-//
-// 68 - 6D:  B pos 1 name
-// 6E - 73:  B pos 2 name
-// 74 - 79:  B pos 3 name
-//
-//
-//
-// SCORES
-// Scores are saved as a literal dec <> hex transformation.
-// Examples:
-// 0x01: "01"
-// 0x99: "99"
-// 0xAB: "AB"
-// 0xFF: "FF"
-//
-// Ranges:
-// 00 - 02:  A pos 1 score
-// 03 - 05:  A pos 2 score
-// 06 - 08:  A pos 3 score
-//
-// 0C - 0E:  B pos 1 score
-// 0F - 11:  B pos 2 score
-// 12 - 14:  B pos 3 score
-//
-//
-//
-// LEVELS
-// Levels are stored as the actual value.
-//
-// Ranges:
-// 18:       A pos 1 level
-// 19:       A pos 2 level
-// 1A:       A pos 3 level
-//
-// 1C:       B pos 1 level
-// 1D:       B pos 2 level
-// 1E:       B pos 3 level
